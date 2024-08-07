9a289e8f060aa3594b288fd0cbd6f07d4ba98e6d
Yanis-mnh <monnah.yans@gmail.com>
2024-08-07 20:34:31 +0100

    nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
+
+const handel_click = (page: number) => {
+  const test = is_page.map((i: boolean) => (i === true ? true : false));
+
+  set_is_page(test);
+  console.log(test);
+};
+
+const [is_page, set_is_page] = useState([true, false, false]);
+console.log(is_page);
 function NavBar() {
   return (
     <nav>
       <ul>
         <li>
           About
-          <div className="active_page"></div>
+          <div
+            className={is_page[0] ? "active_page" : "unactive_page"}
+            onClick={() => {
+              handel_click(0);
+            }}
+          ></div>
         </li>
         <li>
           Contact
-          <div className="active_page"></div>
+          <div
+            className={is_page[1] ? "active_page" : "unactive_page"}
+            onClick={() => {
+              handel_click(1);
+            }}
+          ></div>
         </li>
         <li>
           Project
-          <div className="active_page"></div>
+          <div
+            className={is_page[2] ? "active_page" : "unactive_page"}
+            onClick={() => {
+              handel_click(2);
+            }}
+          ></div>
         </li>
       </ul>
     </nav>
