97b573cd9b8d0ae5e43e9e0860ac6897067654a5
Yanis-mnh <monnah.yans@gmail.com>
2024-08-07 18:05:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Project</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
