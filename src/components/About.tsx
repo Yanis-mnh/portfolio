3b4095e7173730a3883ec5a4f2c372d2a56999a3
Yanis-mnh <monnah.yans@gmail.com>
2024-08-15 20:53:18 +0100

    working on about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,13 +5,12 @@ const About = () => {
       <div className="about" id="about">
         <div className="div1_about_me">
           <p className="about_text">
-            <h2>About me</h2>
-            Hi, I'm Yanis, a master's student in Information Systems and Data.
-            With a strong passion for video game development and software
+            <h2>Hi, I'm Yanis</h2>a master's student in Information Systems and
+            Data. With a strong passion for video game development and software
             creation, I enjoy turning complex problems into simple, elegant
             solutions.
           </p>
-          <img className="logo_high" src="public/icons/logo_high.png" />
+          <img className="logo_high" src="icons/logo_high.png" />
         </div>
         <div className="div2_skills">
           <h2>Skills</h2>
