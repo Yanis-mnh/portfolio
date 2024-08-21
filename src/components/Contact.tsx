c6d7e97e818ef637470f430c4c0bd7ff3e1feb0d
Yanis-mnh <monnah.yans@gmail.com>
2024-08-22 00:48:00 +0100

    change: style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,21 +4,23 @@ const Contact = () => {
   return (
     <>
       <footer className="contact_container" id="contact">
-        <a href="https://www.instagram.com/yanix.code/" target="blank">
-          <img src={baseUrl + "icons/instagram.png"} alt="insta_icon" />
-        </a>
-        <a
-          href="https://www.facebook.com/profile.php?id=61564108437013&locale=fr_FR"
-          target="blank"
-        >
-          <img src={baseUrl + "icons/facebook.png"} alt="insta_icon" />
-        </a>
-        <a target="blank" href="https://github.com/Yanis-mnh">
-          <img src={baseUrl + "icons/github.png"} alt="gmail_icon" />
-        </a>
-        <a href="mailto:yanix.code@gmail.com">
-          <img src={baseUrl + "icons/gmail.png"} alt="gmail_icon" />
-        </a>
+        <div>
+          <a href="https://www.instagram.com/yanix.code/" target="blank">
+            <img src={baseUrl + "icons/instagram.png"} alt="insta_icon" />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=61564108437013&locale=fr_FR"
+            target="blank"
+          >
+            <img src={baseUrl + "icons/facebook.png"} alt="insta_icon" />
+          </a>
+          <a target="blank" href="https://github.com/Yanis-mnh">
+            <img src={baseUrl + "icons/github.png"} alt="gmail_icon" />
+          </a>
+          <a href="mailto:yanix.code@gmail.com">
+            <img src={baseUrl + "icons/gmail.png"} alt="gmail_icon" />
+          </a>
+        </div>
       </footer>
     </>
   );
