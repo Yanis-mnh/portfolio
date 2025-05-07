0cf85555c96b521c257160e2c45c721b0c1f55de
Yanis-mnh <monnah.yans@gmail.com>
2025-05-07 13:25:27 +0100

    changing

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,16 +5,13 @@ const Contact = () => {
     <>
       <footer className="contact_container" id="contact">
         <div>
-          <a
-            href="https://www.facebook.com/profile.php?id=61564108437013&locale=fr_FR"
-            target="blank"
-          >
+          <a href="#" target="blank">
             <img src={baseUrl + "icons/facebook.png"} alt="insta_icon" />
           </a>
           <a target="blank" href="https://github.com/Yanis-mnh">
             <img src={baseUrl + "icons/github.png"} alt="gmail_icon" />
           </a>
-          <a href="mailto:yanix.code@gmail.com">
+          <a href="#">
             <img src={baseUrl + "icons/gmail.png"} alt="gmail_icon" />
           </a>
         </div>
