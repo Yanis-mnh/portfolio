6182dbca01bfd1c2c0cac57aae62546325fa7be9
Yanis-mnh <monnah.yans@gmail.com>
2026-07-22 03:19:05 +0100

    Update deployment workflow and package dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
